bf08604a9efc7f06ecf4f2618214ddac34710df0
DarkEyesTheBaker <julesdietsch@fioptics.com>
2020-10-02 15:54:58 -0400

    One Dimensional Arrays

--- ch11_ex1_MonthSales/src/Console.java
+++ ch11_ex1_MonthSales/src/Console.java
@@ -1,48 +1,76 @@
+import java.lang.Enum;
+import java.time.Month;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Console {
 
-    private static Scanner sc = new Scanner(System.in);
-    
-    public static void displayLine() {
-        System.out.println();
-    }
+	private static Scanner sc = new Scanner(System.in);
 
-    public static void displayLine(String s) {
-        System.out.println(s);
-    }
+	public static void displayLine() {
+		System.out.println();
+	}
 
-    public static String getString(String prompt) {
-        System.out.print(prompt);
-        String s = sc.nextLine();
-        return s;
-    }
+	public class MonthNumber {
+//public enum Month extends Enum<Month> implements TemporalAccessor, TemporalAdjuster {
+public static  void main(String int [monthNumber] args) 	{
+//		ArrayList<String> month = new ArrayList<String>();
+		
+		int [] monthNumber = new month(13); {
+		month[0]=null;
+		month[1]=January;
+		month[2]=February;
+		month[3]=March;
+		month[4]=April;
+		month[5]=May;
+		month[6]=June;
+		month[7]=July;
+		month[8]=August;
+		month[9]=September;
+		month[10]=October;
+		month[11]=November;
+		month[12]=December;
 
-    public static int getInt(String prompt) {
-        int i = 0;
-        while (true) {
-            System.out.print(prompt);
-            try {
-                i = Integer.parseInt(sc.nextLine());
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("Error! Invalid integer. Try again.");
-            }
-        }
-        return i;
-    }
+		int m = Integer.parseInt();System.out.println(month);
+		}
+		}
 
-    public static double getDouble(String prompt) {
-        double d = 0;
-        while (true) {
-            System.out.print(prompt);
-            try {
-                d = Double.parseDouble(sc.nextLine());
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println("Error! Invalid decimal. Try again.");
-            }
-        }
-        return d;
-    }
+		public static void displayLine(String s) {
+			System.out.println(s);
+		}
+
+		public static String getString(String prompt) {
+			System.out.print(prompt);
+			String s = sc.nextLine();
+			return s;
+		}
+
+		public static int getInt(String prompt) {
+			int i = 0;
+			while (true) {
+				System.out.print(prompt);
+				try {
+					i = Integer.parseInt(sc.nextLine());
+					break;
+				} catch (NumberFormatException e) {
+					System.out.println("Error! Invalid integer. Try again.");
+				}
+			}
+			return i;
+		}
+
+		public static double getDouble(String prompt) {
+			double d = 0;
+			while (true) {
+				System.out.print(prompt);
+				try {
+					d = Double.parseDouble(sc.nextLine());
+					break;
+				} catch (NumberFormatException e) {
+					System.out.println("Error! Invalid decimal. Try again.");
+				}
+			}
+			return d;
+		}
+	}
 }
--- ch11_ex1_MonthSales/src/MonthSelectorApp.java
+++ ch11_ex1_MonthSales/src/MonthSelectorApp.java
@@ -2,40 +2,37 @@ import java.text.NumberFormat;
 
 public class MonthSelectorApp {
 
-    public static void main(String[] args) {
-        System.out.println("Monthly Sales\n");
-        
-        // declare monthNames and monthSales arrays
+	public static void main(String[] args) {
+		System.out.println("Monthly Sales\n");
 
+		// declare monthNames and monthSales arrays
 
-        // get currency formatting
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        
-        // get one or more months
-        String choice = "y";
-        while (choice.equalsIgnoreCase("y")) {
-            // get the input from the user
-            int monthNumber = Console.getInt("Enter month number: ");
-            
-            // validate input
-            if (monthNumber < 1 || monthNumber > monthName.length) {
-                Console.displayLine("Invalid month number. Try again.");
-                continue;                
-            }
-            
-            // get the index number for the month
-            // and display the month name and sales            
+		// get currency formatting
+		NumberFormat currency = NumberFormat.getCurrencyInstance();
 
+		// get one or more months
+		String choice = "y";
+		while (choice.equalsIgnoreCase("y")) {
+			// get the input from the user
+			int monthNumber = Console.getInt("Enter month number: ");
 
-            // check if the user wants to continue
-            choice = Console.getString("Continue? (y/n): ");
-            Console.displayLine();
-        }
-        
-        // display the total sales for the year
-        
-        
-        Console.displayLine();
-    }    
+			// validate input
+			if (monthNumber < 1 || monthNumber > 1) {
+				Console.displayLine("Invalid month number. Please try again.");
+				continue;
+			}
+
+			// get the index number for the month
+			// and display the month name and sales
+
+			// check if the user wants to continue
+			choice = Console.getString("Continue? (y/n): ");
+			Console.displayLine();
+		}
+
+		// display the total sales for the year
+
+		Console.displayLine();
+	}
 
 }
