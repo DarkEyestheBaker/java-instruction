148e901541b35e5b1690be6c55db591fbe01c123
DarkEyesTheBaker <julesdietsch@fioptics.com>
2020-10-03 15:57:04 -0400

    LinkedList

--- ch12_QueueTester/src/QueueApp.java
+++ ch12_QueueTester/src/QueueApp.java
@@ -1,18 +1,19 @@
 public class QueueApp {
 
-    public static void main(String args[]) {
+	public static void main(String args[]) {
 
-Queue<String> invoices = new Queue<>();
-invoices.push("Invoice 1");
-invoices.push("Invocie 2");
-invoices.push("Invoice 3");
-System.out.println("The queue contains " + invoices.size() + " invocies");
+		Queue<String> invoices = new Queue<>();
+		invoices.push("Invoice 1");
+		invoices.push("Invocie 2");
+		invoices.push("Invoice 3");
+		System.out.println("The queue contains " + invoices.size() + " invoices");
 
-while (invoices.size() > 0) {
-    String invoice = invoices.pull();
-    System.out.println("Processing: " + invoice);
-}
-System.out.println("The queue contains " + invoices.size() + " invoices");
+		while (invoices.size() > 0) {
+			String invoice = invoices.pull();
+			System.out.println("Processing: " + invoice);
+		}
+		System.out.println();
+		System.out.println("The queue contains " + invoices.size() + " invoices");
 
-    }
+	}
 }
