4f4e02b04cc5ce4d86e3dcf19ccd515007397afc
DarkEyesTheBaker <julesdietsch@fioptics.com>
2020-10-02 20:31:32 -0400

    Batting & Slugging Average

--- ch11_ex1_MonthSales/src/Console.java
+++ ch11_ex1_MonthSales/src/Console.java
@@ -13,64 +13,46 @@ public class Console {
 
 	public class MonthNumber {
 //public enum Month extends Enum<Month> implements TemporalAccessor, TemporalAdjuster {
-public static  void main(String int [monthNumber] args) 	{
-//		ArrayList<String> month = new ArrayList<String>();
-		
-		int [] monthNumber = new month(13); {
-		month[0]=null;
-		month[1]=January;
-		month[2]=February;
-		month[3]=March;
-		month[4]=April;
-		month[5]=May;
-		month[6]=June;
-		month[7]=July;
-		month[8]=August;
-		month[9]=September;
-		month[10]=October;
-		month[11]=November;
-		month[12]=December;
 
-		int m = Integer.parseInt();System.out.println(month);
-		}
-		}
-
-		public static void displayLine(String s) {
-			System.out.println(s);
-		}
-
-		public static String getString(String prompt) {
-			System.out.print(prompt);
-			String s = sc.nextLine();
-			return s;
-		}
-
-		public static int getInt(String prompt) {
-			int i = 0;
-			while (true) {
-				System.out.print(prompt);
-				try {
-					i = Integer.parseInt(sc.nextLine());
-					break;
-				} catch (NumberFormatException e) {
-					System.out.println("Error! Invalid integer. Try again.");
-				}
-			}
-			return i;
-		}
-
-		public static double getDouble(String prompt) {
-			double d = 0;
-			while (true) {
-				System.out.print(prompt);
-				try {
-					d = Double.parseDouble(sc.nextLine());
-					break;
-				} catch (NumberFormatException e) {
-					System.out.println("Error! Invalid decimal. Try again.");
-				}
-			}
-			return d;
-		}
 	}
-}
+
+	}
+
+	public static void displayLine(String s) {
+		System.out.println(s);
+	}
+
+	public static String getString(String prompt) {
+		System.out.print(prompt);
+		String s = sc.nextLine();
+		return s;
+	}
+
+	public static int getInt(String prompt) {
+		int i = 0;
+		while (true) {
+			System.out.print(prompt);
+			try {
+				i = Integer.parseInt(sc.nextLine());
+				break;
+			} catch (NumberFormatException e) {
+				System.out.println("Error! Invalid integer. Try again.");
+			}
+		}
+		return i;
+	}
+
+	public static double getDouble(String prompt) {
+		double d = 0;
+		while (true) {
+			System.out.print(prompt);
+			try {
+				d = Double.parseDouble(sc.nextLine());
+				break;
+			} catch (NumberFormatException e) {
+				System.out.println("Error! Invalid decimal. Try again.");
+			}
+		}
+		return d;
+	}
+}}
