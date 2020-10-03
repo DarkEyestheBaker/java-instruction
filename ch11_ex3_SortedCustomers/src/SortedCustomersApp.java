40557cdac2ca09ec822482e1c52629c082c29d3b
DarkEyesTheBaker <julesdietsch@fioptics.com>
2020-10-03 15:31:15 -0400

    Chapters 11 & 12 Arrays, Collections, and Queues

---
+++ ch11_ex3_SortedCustomers/src/SortedCustomersApp.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class SortedCustomersApp {
+
+	public static void main(String[] args) {
+		Customer[] customers = new Customer[3];
+		customers[0] = new Customer("anne@murach.com ", "Anne ", "Boehm");
+		customers[1] = new Customer("joel@murach.com ", "Joel ", "Murach");
+		customers[2] = new Customer("mike@murach.com ", "Mike ", "Murach");
+
+		Arrays.sort(customers);
+
+		for (Customer c : customers) {
+			System.out.println(c.getEmail() + "  " + c.getFirstName() + " " + c.getLastName());
+		}
+	}
+}
