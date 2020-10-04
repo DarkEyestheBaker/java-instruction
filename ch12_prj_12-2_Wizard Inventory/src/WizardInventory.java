69bd17f5f59a81f160f0932b2e975597f13593b5
DarkEyesTheBaker <julesdietsch@fioptics.com>
2020-10-04 19:33:15 -0400

    Wizard Inventory ArrayList

--- ch12_prj_12-2_Wizard Inventory/src/WizardInventory.java
+++ ch12_prj_12-2_Wizard Inventory/src/WizardInventory.java
@@ -6,23 +6,24 @@ public class WizardInventory {
 	private String edit;
 	private String drop;
 	private String exit;
-	public () {
-		
+	{
+
 	}
-	//Inventory
+
+	// Inventory
 	public static void main(String[] args) {
 		ArrayList<String> wizInv = new ArrayList<>(4);
 		wizInv.add("Tome of the Wizards");
 		wizInv.add("Ring of Spell Storing");
 		wizInv.add("Cloak of Displacement");
-		 wizInv.add("Illusionist's Bracers");
+		wizInv.add("Illusionist's Bracers");
 
-		 //Edit Inventory - Remove one item and add Rod of Stewart
-		 String wizInv = wizInv.remove();
-		 
-		 System.out.println("One " + wizInv + "was banished from this realm!  And " + wizInv.add("Rod of Stewart") + " was given a place of honor!");
-		 
-					
+		// Edit Inventory - Remove one item and add Rod of Stewart
+		String wizInv = wizInv.remove();
+
+		System.out.println("One " + wizInv + "was banished from this realm!  And " + wizInv.add("Rod of Stewart")
+				+ " was given a place of honor!");
+
 //Print lines - no input needed
 		System.out.println("Welcome to the Wizard Inventory Game!");
 		System.out.println();
@@ -33,13 +34,26 @@ public class WizardInventory {
 		System.out.println("drop - Drop an item");
 		System.out.println("exit - Exit program");
 
-	}
-	public  {
+	}public
+
+	{
 		this.command = command;
 	}
-	
+
 	// get the input from the user
 	System.out.println("Your command? ");
-	if (this.)
-
-}
+	// Error message for bad user input
+	System.out.println("No, fool!  Try again!");if(this.)
+	{ //show
+	}else if
+	{ // grab
+	}else if
+	{//edit
+	}else if
+	{// drop
+	}else
+	{// exit
+	}
+	{
+	}System.out.println("Faretheewell and safe travels, adventurer!");
+}}
