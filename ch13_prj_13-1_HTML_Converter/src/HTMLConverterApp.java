129f39188de9e3720624e72700f0f4f931f854a5
DarkEyesTheBaker <julesdietsch@fioptics.com>
2020-10-07 18:48:40 -0400

    HTML Converter with IndexOf()

--- ch13_prj_13-1_HTML_Converter/src/HTMLConverterApp.java
+++ ch13_prj_13-1_HTML_Converter/src/HTMLConverterApp.java
@@ -1,30 +1,30 @@
-import java.util.HashMap;
-
 public class HTMLConverterApp {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the HTML Converter!");
-		private static String 
-		String  grocList  = "<h1>Grocery List</h1>\n" +
-				"<ul>\n" +
-				" <li>Eggs</li>\n" +
-				" <li>Milk</li>\n" +
-				" <li>Butter</li>\n" +
-				"</ul>";
-		String[] grocListArray = grocList.split("  ");
-		System.out.println("grocListArray length = " + grocListArray.length);
-		for (String g : grocListArray) {
-			System.out.println(g);
-		System.out.println();
+		String html = "<h1>Grocery List</h1>\n" + "<ul>\n" + " <li>Eggs</li>\n" + " <li>Milk</li>\n"
+				+ " <li>Butter</li>\n" + "</ul>";
+
+		System.out.println("HTML Input: \n" + html);
+
+//		Remove  spaces before tags
+		while (html.indexOf(" <") != -1) {
+			html = html.replace(" <", "<");
 		}
-
-
-		
-		
-		System.out.println("INPUT");
-		
-		
-		System.out.println("OUTPUT");
+//Replace list items with asterisks
+		html = html.replace("<li>", "* ");
+//		Remove HTML tags
+		int beginTagIndex = 0;
+		while (beginTagIndex != -1) {
+			beginTagIndex = html.indexOf("<");
+			if (beginTagIndex != -1) {
+				int endTagIndex = html.indexOf(">", beginTagIndex + 1);
+				String beforeTag = html.substring(0, beginTagIndex);
+				String afterTag = html.substring(endTagIndex + 1);
+				html = beforeTag + afterTag;
+			}
+		}
+		// Remove blank lines
+		html = html.replace("\n\n", "\n");
+		System.out.println("\nText Output: \n" + html);
 	}
-
 }
