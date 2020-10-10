1310a8ae778593da43caae05be8436ba77b0e9dd
DarkEyesTheBaker <julesdietsch@fioptics.com>
2020-10-10 08:54:08 -0400

    Email Creator - Trim, Upper/Lowercases

--- ch13_prj_13-2_EmailCreator/src/dietsch/app/EmailCreatorApp.java
+++ ch13_prj_13-2_EmailCreator/src/dietsch/app/EmailCreatorApp.java
@@ -1,32 +1,39 @@
 package dietsch.app;
 
-import java.util.Locale;
-
 public class EmailCreatorApp {
 
-	public static void main(String[] args, Locale length) {
+	public static String convertToTitleCase(String inputStr) {
+		String firstNameTrimmed = inputStr.trim();
+		String nameFirstLetter = firstNameTrimmed.substring(0, 1);
+		nameFirstLetter = nameFirstLetter.toUpperCase();
 
-		String[] csv = { " james,butler ,jbutler@gmail.com", "Josephine,Darakjy,Josephine_Darakjy@darakjy.org",
-				"ART,VENERE,ART@VENERE.ORG" };
+		String nameAfterFirstLetter = firstNameTrimmed.substring(1);
+		nameAfterFirstLetter = nameAfterFirstLetter.toLowerCase();
 
-		String template = "To: {email}\n" + "From: noreply@deals.com\n" + "Subject: Deals!\n\n" + "Hi {first_name},\n\n"
-				+ "We've got some great deals for you. Check our website!";
-		for (String currLine : csv) {
-			System.out.println("=======================================================");
-			String[] nameParts = currLine.split(",");
+		String titleCaseName = nameFirstLetter + nameAfterFirstLetter;
 
-			String firstName = firstName.trim();
-			String firstLtr = firstLtr.toUpperCase(0);
-			String restLtr = restLtr.toLowerCase(length);
-			String email = email.toLowerCase(length);
-			String email = template.replace("{email}", nameParts[2]);
-			String firstName = email.replace("{first_name}", nameParts[0]);
-			System.out.println();
-			System.out.println("first_name}" + firstLtr + restLtr);
-			System.out.println();
-
-		}
-
+		return titleCaseName;
 	}
 
+	public static void main(String[] args) {
+		String[] csv = { "   james   ,butler,jbutler@gmail.com", "Josephine,Darakjy,Josephine_Darakjy@darakjy.org",
+				"ART,VENERE,ART@VENERE.ORG" };
+
+		String template = "To:      {email}\n" + "From:    noreply@deals.com\n" + "Subject: Deals!\n\n"
+				+ "Hi {first_name},\n\n" + "We've got some great deals for you. Check our website!";
+
+		System.out.println("Email Creator");
+		System.out.println();
+
+		for (String curEntry : csv) {
+
+			String[] nameParts = curEntry.split(",");
+
+			String emailAddress = nameParts[2].toLowerCase();
+
+			String firstNameTitleCase = convertToTitleCase(nameParts[0]);
+
+			System.out.println(firstNameTitleCase);
+		}
+	}
 }
